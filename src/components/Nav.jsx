f56ffa432721a52e57185bff918cad62df932ac8
Parker Parham <parkerparham@parkers-mbp.lan>
2023-09-14 14:22:18 -0400

    completes navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,9 +19,33 @@ export default function Nav() {
               </div>
               <ul className="flex items-center font-semibold space-x-4">
                 <li><a href="#" className='hover:text-gray-400'>Pull Requests</a></li>
+                <li><a href="#" className='hover:text-gray-400'>Issues</a></li>
+                <li><a href="#" className='hover:text-gray-400'>Marketplace</a></li>
+                <li><a href="#" className='hover:text-gray-400'>Explore</a></li>
               </ul>
             </div>
-            <div>right</div>
+            <div className="flex items-center space-x-4 text-white">
+              <a href="#" className='relative hover:text-gray-400'>
+                <svg className="w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                </svg>
+                <div className="bg-blue-500 rounded-full w-2 h-2 absolute top-0 right-0"></div>
+              </a>
+              <a href="#" className='flex items-center hover:text-gray-400'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 ml-1">
+                  <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" clipRule="evenodd" />
+                </svg>
+              </a>
+              <a href="#" className='flex items-center hover:text-gray-400'>
+                <img src="https://avatars.githubusercontent.com/u/102374286?v=4" alt="avatar" className='w-5 h-5 rounded-full' />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 ml-1">
+                  <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" clipRule="evenodd" />
+                </svg>
+              </a>
+            </div>
         </nav>
         </div> 
     </>
